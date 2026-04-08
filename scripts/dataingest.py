49d3a8a42758e8e6cd34d8bb67c893aceb1eeef9
vewilya <vewilya@me.com>
2026-04-08 16:04:51 +0200

    removed unused import

--- scripts/dataingest.py
+++ scripts/dataingest.py
@@ -1,20 +1,10 @@
-import pandas as pd
-
-
 def ingest_data(
-        engine,
-        data,
-        target_table: str,
-        chunksize: int = 100000,):
+    engine,
+    data,
+    target_table: str,
+    chunksize: int = 100000,
+):
 
-    data.head(0).to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="replace")
+    data.head(0).to_sql(name=target_table, con=engine, if_exists="replace")
 
-    data.to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="append",
-        chunksize = chunksize
-    )
+    data.to_sql(name=target_table, con=engine, if_exists="append", chunksize=chunksize)
